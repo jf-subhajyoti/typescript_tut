a8408a395a709af1e1962d3198547563b9eb0fc7
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-11 22:53:40 +0530

    Intro

---
+++ 01intro/intro.ts
@@ -0,0 +1,9 @@
+let greetings : string = "Hello Subh";
+console.log(greetings);
+
+let isLoggedIn : boolean = true;
+
+let userId = 5427; // typescript itself understands that it is a number
+// userId = 'subh'
+
+export {};
