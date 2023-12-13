3f707d7f331db3364b980f0730bb508c0cfdd96c
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-13 22:31:16 +0530

    Array basics

---
+++ 02Basics/myArray.ts
@@ -0,0 +1,14 @@
+const superHeros : string[] = ['SpiderMan', 'Batman', 'SuperMan'];
+
+type User = {
+    name: string,
+    age: number
+}
+
+const user : Array<User> = [{name: 'subh', age: 25}, {name: 'akash', age: 24}]
+
+
+
+// const  (string | number)[] = []
+
+export {}
