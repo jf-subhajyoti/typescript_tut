1e22bddb72b1f632bc9e0efa5ab172b9f6862ce5
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-13 22:12:30 +0530

    Object basics

--- 02Basics/myObjects.ts
+++ 02Basics/myObjects.ts
@@ -11,3 +11,34 @@ createUser({name: 'subh', isPaid: false});
 function createCourse():{name: string, price: number}{
     return ({ name: 'typescript', price: 299 });
 }
+
+type User = {
+    readonly _id: string,
+    name: string,
+    email: string,
+    isActive: boolean,
+    creditCard?: string
+}
+
+function createUser1(user: User) {
+
+}
+
+createUser1({_id: '1', name: 'subh', email: 'subh@gmail.com', isActive: true})
+
+var newUser : User = {
+    _id: '2',
+    name: 'h',
+    email: 'h@h.com',
+    isActive: false
+}
+
+type cardNumber = {
+    cardNo: string
+}
+
+type cardName = {
+    name: string
+}
+
+type cardDetails = cardNumber & cardName
