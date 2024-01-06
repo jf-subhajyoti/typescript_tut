9b28da24d780b59816da2e7c4f53c3133b5a4d01
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2024-01-06 16:30:20 +0530

    interface

---
+++ 03PureTs/myInterface.ts
@@ -0,0 +1,21 @@
+// interface TakePhoto {
+//     cameraMode: string,
+//     filter: string,
+//     burst: number,
+// }
+
+// interface CreateStory {
+//     createStory(): void
+// }
+
+// class Instagram implements TakePhoto, CreateStory {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst: number,
+//     ) {}
+
+//     createStory(): void {
+//         console.log("Create Story");
+//     }
+// }
